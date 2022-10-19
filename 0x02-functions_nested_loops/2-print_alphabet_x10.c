ab213cc27c3e182bc6843bd1f83973d21eac6df8
nati2007 <nati.piko2014@gmail.com>
2022-10-19 13:29:28 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet x10 times
+ *
+ * Return: void
+ */
+{
+	char c;
+	int  i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+
+		{
+			_putchar(c);
+		}
+
+
+		_putchar('\n');
+
+		i++;
+	}
+}

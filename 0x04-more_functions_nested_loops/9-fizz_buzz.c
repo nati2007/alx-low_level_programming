ba6fbcfeddecb490f31cfb4e4aad76d4e8366804
nati2007 <nati.piko2014@gmail.com>
2022-10-21 18:38:20 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include  <stdio.h>
+
+/**
+ * main - fizz Buzz code
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FuzzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fuzz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

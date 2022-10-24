c840918af60a865cd0159a20e81d8c9ef66a4fbb
nati2007 <nati.piko2014@gmail.com>
2022-10-24 06:21:43 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,18 @@
+#include "MAIN.H"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX Students
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	 char *str;
+	 int len;
+
+	 str = "MAIN!";
+	 len = _strlen(str);
+	 printf("%d\n", len);
+	 return (0);
+}

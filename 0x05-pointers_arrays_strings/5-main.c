d0541df08acd35d3b8d7365d5f0296014b17b752
nati2007 <nati.piko2014@gmail.com>
2022-10-25 01:49:17 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,17 @@
+#include "MAIN.H"
+#include <stdio.h>
+
+/**
+ * main - check the code for Alx Students
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s[10] = "MAIN";
+
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
+}
